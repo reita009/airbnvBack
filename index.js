eda168d1d5e80619fa08faf842c2f91e4e3b7db1
RamiroVidal26 <110636851+RamiroVidal26@users.noreply.github.com>
2023-11-13 15:18:46 -0300

    next

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const User = require("./models/User");
 require("dotenv").config();
 const app = express();
+
+const salt = bcrypt.genSaltSync(10);
 
 app.use(express.json());
 app.use(
@@ -19,9 +22,15 @@ app.get("/test", (req, res) => {
   res.json("test ok");
 });
 
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
   const { userName, email, password } = req.body;
-  res.json({ userName, email, password });
+  const user = await User.create({
+    userName,
+    email,
+    password: bcrypt.hashSync(password, salt),
+  });
+
+  res.json(user);
 });
 
 app.listen(4000);
